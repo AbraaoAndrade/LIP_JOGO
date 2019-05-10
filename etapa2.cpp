24c226ec0f47ebbc1e3bae140b5ab0774f9507e8
lukandrad <42744357+lukandrad@users.noreply.github.com>
2019-05-10 16:05:10 -0300

    Update etapa2.cpp

--- etapa2.cpp
+++ etapa2.cpp
@@ -5,13 +5,14 @@ int main() {
         //tamanho da tela
     int xtela = 640, ytela = 480;
         //movimento
-    float xj = 220, yj = 190, xi = 370, yi =  190,v = 2;
-            //pulo
-    float puloy = 0, contframes = 0;
+            //jogador
+    float xj = 220, yj = 190, vj = 5;
+            //inimigo
+    float xi =220, yi = 190, vi = 5;
+        //pulo
     bool pulo = false;
-
-
-
+    float yp = 0;
+    int contFrames = 0;
 
     //cria a janela com tamanho 640x480 pixels
     SFML sfml(xtela,ytela,"Meu jogo!");
@@ -19,61 +20,59 @@ int main() {
     //define a cor de fundo como preto
     sfml.background(0,0,0);
 
-    //início do laço principal do jogo
+    //inÃ­cio do laÃ§o principal do jogo
     while (sfml.windowIsOpen()) {
 
-        //apaga o conteúdo da janela
+        //apaga o conteÃºdo da janela
         sfml.clear();
 
-        //movimento
-            //movimento inimigo
-        xi+=v;
-            //movimento por teclado jogador
+        //MOVIMENTO -----------------------------------------------------
+            //JOGADOR
         if (sfml.keyIsDown(SFML::Key::Left)) {  //seta esquerda
-            xj-=v;
+            xj-=vj;
         }
         if (sfml.keyIsDown(SFML::Key::Right)) {   //seta direita
-            xj+=v;
+            xj+=vj;
         }
                 //pulo
-        if(sfml.keyIsDown(SFML::Key::Up) && pulo == false){
+        if(sfml.keyIsDown(SFML::Key::Up) && pulo == false) {
             pulo = true;
-            contframes = 0;
+            contFrames = 0;
         }
-        if(pulo == true){
-            contframes++;
-            puloy = 10*(contframes)*(contframes - 10.7308673997732);
-            if(puloy>0){
-                puloy = 0;
+        if(pulo = true) {
+            contFrames++;
+            yp = 0.5*(contFrames)*(contFrames - 30);
+            if(yp > 0) {
                 pulo = false;
+                yp = 0;
             }
         }
-
-        //limites das extremidades
-            //jogador
+                //limites das extremidades
         if(xj>xtela){
             xj = -100;
         }
         if(xj<-100){
             xj = xtela;
         }
-            //inimigo
+            //INIMIGO
+        xi+=vi;
+                //limites das extremidades
         if(xi>xtela){
             xi = -100;
         }
         if(xi<-100){
-            xi = xtela;
+            xj = xtela;
         }
 
-        //desenha formas geometricas
-            //quadrado
-        sfml.fill(255, 0, 0);
-        sfml.rect(xi, yi, 100, 100);
-            //elipse
+        //DESENHA PERSONAGENS --------------------------------------------
+            //JOGADOR
         sfml.fill(155, 0, 0);
-        sfml.ellipse(xj, yj+puloy, 100, 100);
+        sfml.ellipse(xj, yj+yp, 100, 100);
+            //INIMIGO
+        sfml.fill(115, 0, 0);
+        sfml.ellipse(xi, yi, 100, 100);
 
-        //mostra o conteúdo na janela
+        //mostra o conteÃºdo na janela
         sfml.display();
 
 
@@ -82,3 +81,4 @@ int main() {
 
     return 0;
 }
+
